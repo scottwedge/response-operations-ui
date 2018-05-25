a3845e18494d66fa749063da040c9e04ced8b077
James Gardiner <james@jgardiner.co.uk>
2018-05-25 11:34:41 +0100

    Update talisman implementation

--- response_operations_ui/__init__.py
+++ response_operations_ui/__init__.py
@@ -9,48 +9,103 @@ import redis
 
 from response_operations_ui.cloud.cloudfoundry import ONSCloudFoundry
 from response_operations_ui.logger_config import logger_initial_config
+from response_operations_ui.talisman import Talisman
 from response_operations_ui.user import User
+
+
+CSP_POLICY = {
+    "default-src": ["'self'", "https://cdn.ons.gov.uk"],
+    "style-src": [
+        "'self'",
+        "https://cdn.ons.gov.uk",
+        "https://maxcdn.bootstrapcdn.com",
+    ],
+    "font-src": [
+        "'self'",
+        "data:",
+        "https://cdn.ons.gov.uk",
+        "https://fonts.gstatic.com",
+    ],
+    "script-src": [
+        "'self'",
+        "https://www.google-analytics.com",
+        "https://cdn.ons.gov.uk",
+        "http://code.jquery.com",
+    ],
+    "connect-src": [
+        "'self'",
+        "https://www.google-analytics.com",
+        "https://cdn.ons.gov.uk",
+    ],
+    "img-src": [
+        "'self'",
+        "data:",
+        "https://www.google-analytics.com",
+        "https://cdn.ons.gov.uk",
+    ],
+}
 
 app = Flask(__name__)
 
 # Load css and js assets
 assets = Environment(app)
 assets.url = app.static_url_path
-scss_min = Bundle('css/*', 'css/fonts/*', 'css/components/*',
-                  filters=['cssmin'], output='minimised/all.min.css')
-assets.register('scss_all', scss_min)
-js_min = Bundle('js/*', filters='jsmin', output='minimised/all.min.js')
-assets.register('js_all', js_min)
+scss_min = Bundle(
+    "css/*",
+    "css/fonts/*",
+    "css/components/*",
+    filters=["cssmin"],
+    output="minimised/all.min.css",
+)
+assets.register("scss_all", scss_min)
+js_min = Bundle("js/*", filters="jsmin", output="minimised/all.min.js")
+assets.register("js_all", js_min)
 
-app_config = 'config.{}'.format(os.environ.get('APP_SETTINGS', 'Config'))
+app_config = "config.{}".format(os.environ.get("APP_SETTINGS", "Config"))
 app.config.from_object(app_config)
 
 app.url_map.strict_slashes = False
-app.secret_key = app.config['RESPONSE_OPERATIONS_UI_SECRET']
+app.secret_key = app.config["RESPONSE_OPERATIONS_UI_SECRET"]
 
-logger_initial_config(service_name='response-operations-ui', log_level=app.config['LOGGING_LEVEL'])
+logger_initial_config(
+    service_name="response-operations-ui", log_level=app.config["LOGGING_LEVEL"]
+)
 logger = logging.getLogger(__name__)
 
 login_manager = LoginManager(app)
 login_manager.init_app(app)
 login_manager.login_view = "sign_in_bp.sign_in"
 
-if app.config['SESSION_TYPE'] == 'redis':
+if app.config["SESSION_TYPE"] == "redis":
     cf = ONSCloudFoundry()
     # If deploying in cloudfoundry set config to use cf redis instance
     if cf:
-        redis_service_name = app.config['REDIS_SERVICE']
-        logger.info('Cloudfoundry detected, setting service configurations')
+        redis_service_name = app.config["REDIS_SERVICE"]
+        logger.info("Cloudfoundry detected, setting service configurations")
         redis_service = cf.redis(redis_service_name)
-        app.config['REDIS_HOST'] = redis_service.credentials['host']
-        app.config['REDIS_PORT'] = redis_service.credentials['port']
+        app.config["REDIS_HOST"] = redis_service.credentials["host"]
+        app.config["REDIS_PORT"] = redis_service.credentials["port"]
 
-    redis = redis.StrictRedis(host=app.config['REDIS_HOST'],
-                              port=app.config['REDIS_PORT'],
-                              db=app.config['REDIS_DB'])
+    redis = redis.StrictRedis(
+        host=app.config["REDIS_HOST"],
+        port=app.config["REDIS_PORT"],
+        db=app.config["REDIS_DB"],
+    )
 
     # wrap in the flask server side session manager and back it by redis
-    app.config['SESSION_REDIS'] = redis
+    app.config["SESSION_REDIS"] = redis
+
+Talisman(
+    app,
+    content_security_policy=CSP_POLICY,
+    content_security_policy_nonce_in=["script-src"],
+    session_cookie_secure=app.config["SECURE_COOKIES"],
+    force_https=False,  # this is handled at the firewall
+    strict_transport_security=True,
+    strict_transport_security_max_age=31536000,
+    strict_transport_security_include_subdomains=True,
+    frame_options="DENY",
+)
 
 Session(app)
 
